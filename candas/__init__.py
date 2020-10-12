e006174e95734c7d96a7be5453eb9f6833dd487a
JulianW <julian@wagenschuetz.com>
2020-10-12 20:02:56 +0200

    Drop SQL functionalities

--- candas/__init__.py
+++ candas/__init__.py
@@ -1,3 +1,2 @@
 """ __init__ module for candas package """
-from . dataframe import from_file, from_database, load_dbc, from_fake
-from . database import initialize_database
+from . dataframe import from_file, load_dbc, from_fake
